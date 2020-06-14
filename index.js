b612fdcd1fad6178162faf90a42d4a467d58bfa7
gksqlc40 <gksqlc40@gmail.com>
2020-06-14 16:29:56 +0900

    .

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  mul: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-};
+const title = document.querySelector("#title");
 
-const plus = calculator.plus(7, 3);
-const minus = calculator.minus(7, 3);
-const mul = calculator.mul(7, 3);
-const divide = calculator.divide(7, 3);
-console.log("plus", plus, minus, mul, divide);
+const CLICKED_CLASS = "clicked";
+
+function handleclick() {
+  title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() {
+  title.addEventListener("click", handleclick);
+}
+init();
